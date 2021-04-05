ce897c8197d85a43667a1f5d5f1499a7b1d74a21
cristianS97 <cristiansaez97@gmail.com>
2021-04-04 20:43:07 -0400

    Modificación para scrapear animeflv y jkanime

--- anime_alerta.py
+++ anime_alerta.py
@@ -5,70 +5,115 @@ import random
 from win10toast import ToastNotifier # pip install win10toast
 import json
 
-# URL de consulta
-url = 'https://jkanime.net/'
+# Función para scrapear jkanime
+def scrap_jk(anime_web):
+    titulo = anime_web.find('h5').text.strip()
+    episodio = anime_web.find('h6').text.strip().replace(' ', '').replace('\n', ' ')
 
-# User-agent tor
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0'
-}
+    return titulo, episodio
 
-# Nombres de los animes junto al día de consulta
-dia = time.strftime('%A').lower()
+# Función para scrapear animeflv
+def scrap_flv(anime_web):
+    titulo = anime_web.find('strong').text
+    episodio = anime_web.find('span', class_="Capi").text
 
-# Lectura del fichero de configuración
-with open('conf/anime_alerta/animes.json') as file:
-    animes = json.load(file)
+    return titulo, episodio
 
-# Animes a consultar, los pasamos todos a minusculas
-animes = [anime.lower() for anime in animes[dia]]
-animes_encontrados = list()
+# Función para dar el mensaje de windows
+def toast_message(titulo, episodio, pagina):
+    # Objeto para interactuar con las notificaciones de windows
+    toaster = ToastNotifier()
+    mensaje = f'{time.strftime("%H:%M:%S")} -> Estrenado\nPágina: {pagina}'
+    toaster.show_toast(f'{titulo} - {episodio}', mensaje, duration=5)
 
-# Objeto para interactuar con las notificaciones de windows
-toaster = ToastNotifier()
+# Función para mostrar el mensaje por consola
+def mensaje_consola(titulo, episodio, pagina, animes_encontrados):
+    animes_encontrados.append(titulo.lower())
+    print(titulo)
+    print(episodio)
+    print(pagina)
+    print('=' * 80)
+    print()
 
-# Ciclo que consultara hasta encontar el anime solicitado
-while True:
-    # Consultamos la url
-    response = requests.get(url, headers=headers)
-    print(f'{time.strftime("%H:%M:%S")} Estado respuesta: {response.status_code}')
-    # Creamos el árbol html
-    soup = BeautifulSoup(response.text)
+if __name__ == '__main__':
+    # URL de consulta
+    urls = [
+        'https://jkanime.net/',
+        'https://www3.animeflv.net/'
+    ]
 
-    # Buscamos donde se encuentran los animes
-    programacion = soup.find(id='slider3')
-    animes_web = programacion.find_all('a', class_='odd')
+    # User-agent tor
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
 
-    # Variable para comprobar si se encontro le anime buscado
-    encontrado = False
-    # Recorremos la lista de animes encontrados
-    for anime_web in animes_web:
-        # Obtenemos sus datos
-        titulo = anime_web.find('h2').text.strip()
-        episodio = anime_web.find('span', class_='episode').text.split('\n')[-1].strip()
-        estreno = anime_web.find('i', class_='clock-icon').text.strip()
+    # Nombres de los animes junto al día de consulta
+    dia = time.strftime('%A').lower()
 
-        # Consultamos si se encontraron los animes deseados
-        for anime in animes:
-            if anime.lower() in titulo.lower() and anime.lower() not in animes_encontrados:
-                encontrado = True
-                animes_encontrados.append(titulo.lower())
-                print(titulo)
-                print(episodio)
-                print(estreno)
-                print('=' * 80)
-                print()
+    # Lectura del fichero de configuración
+    with open('conf/anime_alerta/animes.json') as file:
+        animes = json.load(file)
 
-            if encontrado:
-                # Mensaje que contiene la hora en que se encontró el anime
-                mensaje = f'{time.strftime("%H:%M:%S")} -> Estrenado'
-                toaster.show_toast(f'{titulo} - {episodio}', mensaje, duration=5)
+    # Animes a consultar, los pasamos todos a minusculas
+    animes = [anime.lower() for anime in animes[dia]]
+    animes_encontrados = list()
 
-            encontrado = False
+    # Ciclo que consultara hasta encontar el anime solicitado
+    while True:
+        for url in urls:
+            # Consultamos la url
+            response = requests.get(url, headers=headers)
+            print(f'{time.strftime("%H:%M:%S")} Estado respuesta: {response.status_code}')
+            # Creamos el árbol html
+            soup = BeautifulSoup(response.text)
 
-    if len(animes) == len(animes_encontrados):
-        break
+            if 'jkanime' in url:
+                # Buscamos donde se encuentran los animes
+                programacion = soup.find('div', class_='listadoanime-home')
+                animes_web = programacion.find_all('a', class_='bloqq')
 
-    time.sleep(random.uniform(6.0, 8.0))
+                # Variable para comprobar si se encontro le anime buscado
+                encontrado = False
+                # Recorremos la lista de animes encontrados
+                for anime_web in animes_web:
+                    # Obtenemos sus datos
+                    titulo, episodio = scrap_jk(anime_web)
+
+                    # Consultamos si se encontraron los animes deseados
+                    for anime in animes:
+                        if anime.lower() in titulo.lower() and anime.lower() not in animes_encontrados:
+                            encontrado = True
+                            mensaje_consola(titulo, episodio, 'jkanime', animes_encontrados)
+
+                        if encontrado:
+                            # Mensaje que contiene la hora en que se encontró el anime
+                            toast_message(titulo, episodio, 'jkanime')
+                        encontrado = False
+            else:
+                # Buscamos donde se encuentran los animes
+                programacion = soup.find('ul', class_='ListEpisodios AX Rows A06 C04 D03')
+                animes_web = programacion.find_all('li')
+                # Variable para comprobar si se encontro le anime buscado
+                encontrado = False
+                # Recorremos la lista de animes encontrados
+                for anime_web in animes_web:
+                    # Obtenemos sus datos
+                    titulo, episodio = scrap_flv(anime_web)
+
+                    # Consultamos si se encontraron los animes deseados
+                    for anime in animes:
+                        if anime.lower() in titulo.lower() and anime.lower() not in animes_encontrados:
+                            encontrado = True
+                            mensaje_consola(titulo, episodio, 'animeflv', animes_encontrados)
+
+                        if encontrado:
+                            # Mensaje que contiene la hora en que se encontró el anime
+                            toast_message(titulo, episodio, 'animeflv')
+
+                        encontrado = False
+            time.sleep(random.uniform(3.0, 4.0))
+
+        if len(animes) == len(animes_encontrados):
+            break
 
 print("Fin del programa")
